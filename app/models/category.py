e792a2a172b9eae6b38a77f2eed74bfe4f24349c
huangkai <18778335525@163.com>
2019-09-16 23:08:04 +0800

    添加模型

---
+++ app/models/category.py
@@ -0,0 +1,12 @@
+from pydantic import Schema
+from pydantic.dataclasses import dataclass
+
+
+@dataclass
+class ProductCategory:
+    """商品分类"""
+    parent_id: int = Schema(0)
+    cat_name: str = Schema(..., max_length=64)
+    cat_level: int = Schema(...)  # 分类等级  0 --> 1级;  1 --> 2级
+    cat_keywords: str = Schema(None, max_length=255)  # 分类关键词
+    cat_desc: str
