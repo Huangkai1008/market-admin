849e6215d74d656d388a9974fe3e393803cb7f77
huangkai <18778335525@163.com>
2019-10-11 10:39:37 +0800

    feat: 增加查询商品分类接口验证

--- app/api/v1/endpoints/category.py
+++ app/api/v1/endpoints/category.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 
 from app.db.queries import category as category_api
 from app.models.category import ProductCategory
@@ -10,7 +10,12 @@ router = APIRouter()
 
 
 @router.get('/', response_model=List[ProductCategory])
-async def get_product_categories(page: int = 1, size: int = 10, *, parent_id: int = 0):
+async def get_product_categories(
+    page: int = Query(1, ge=1),
+    size: int = Query(10, ge=1),
+    *,
+    parent_id: int = Query(0, ge=0)
+):
     """获取产品分类列表"""
     categories = await category_api.get_categories(page, size, parent_id=parent_id)
     return categories
