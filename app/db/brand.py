7fd5f1aeb3ebc7677671bb25a8fd698f5a7b1d32
huangkai <18778335525@163.com>
2019-10-19 19:23:12 +0800

    feat: 修改商品品牌模型表

--- app/db/brand.py
+++ app/db/brand.py
@@ -1,9 +1,6 @@
-import json
-
 from tortoise import fields
 from tortoise.models import Model
 
-from app import utils
 
 __all__ = ['Brand']
 
@@ -13,7 +10,4 @@ class Brand(Model):
 
     id = fields.IntField(pk=True)
     name = fields.CharField(max_length=64)  # 品牌名称
-    cat_ids = fields.JSONField(
-        encoder=utils.ExtendedEncoder, decoder=json.decoder
-    )  # 品牌涵盖的分类ids
-    logo = fields.CharField(max_length=255)  # 品牌logo
+    logo = fields.CharField(null=True, max_length=255)  # 品牌logo
