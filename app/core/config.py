e252d1f2aab9ae90a027bfb1409c4782cd594e4d
huangkai <18778335525@163.com>
2019-11-22 22:57:52 +0800

    feat: 使用loguru替换原生logging

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,11 @@
+import logging
+import sys
+
+from loguru import logger
 from starlette.config import Config
 from starlette.datastructures import Secret
+
+from app.core.logging import InterceptHandler
 
 config = Config('../.env')
 
@@ -15,3 +21,10 @@ DB_PASSWORD: Secret = config('DB_PASSWORD', cast=Secret)
 DB_HOST: str = config('DB_HOST')
 DB_PORT: int = config('DB_PORT', cast=int)
 DATABASE: str = config('DB_DATABASE')
+
+# ================ Logging ================
+LOGGING_LEVEL: str = config('LOGGING_LEVEL')
+logging.basicConfig(
+    handlers=[InterceptHandler(level=LOGGING_LEVEL)], level=LOGGING_LEVEL
+)
+logger.configure(handlers=[{'sink': sys.stderr, 'level': LOGGING_LEVEL}])
