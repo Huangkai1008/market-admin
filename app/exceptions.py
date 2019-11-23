36d712629cf4795eeea890b0cd1b4c179f738997
huangkai <18778335525@163.com>
2019-11-23 12:14:32 +0800

    refactor: 配置错误处理

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,7 +1,16 @@
-from typing import Any
+import traceback
+from typing import Any, Union
 
-from fastapi import HTTPException
-from starlette.status import HTTP_400_BAD_REQUEST
+from loguru import logger
+from pydantic import ValidationError
+from starlette.requests import Request
+from starlette.responses import JSONResponse
+from starlette.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_422_UNPROCESSABLE_ENTITY,
+    HTTP_500_INTERNAL_SERVER_ERROR,
+)
+from fastapi.exceptions import RequestValidationError, HTTPException
 
 
 class BadRequestException(HTTPException):
@@ -19,3 +28,37 @@ class GetHardwareIdFailedException(Exception):
     """获取系统硬件信息错误"""
 
     pass
+
+
+async def page_not_found_handler(_: Request, exc: HTTPException) -> JSONResponse:
+    """page not found"""
+    logger.warning(exc.detail)
+    return JSONResponse(dict(message='page not found'), status_code=exc.status_code)
+
+
+async def bad_request_handler(_: Request, exc: BadRequestException) -> JSONResponse:
+    """Bad Request"""
+    logger.warning(exc.detail)
+    return JSONResponse(dict(message=exc.detail), status_code=HTTP_400_BAD_REQUEST)
+
+
+async def validation_exception_handler(
+    _: Request, exc: Union[RequestValidationError, ValidationError]
+) -> JSONResponse:
+    """前端输入实体错误"""
+    errors = exc.errors()
+    message = errors[0]['msg']
+    logger.warning(errors)
+
+    return JSONResponse(
+        dict(message=message), status_code=HTTP_422_UNPROCESSABLE_ENTITY
+    )
+
+
+async def server_error_handler(_: Request, __: HTTPException) -> JSONResponse:
+    """服务器错误"""
+    logger.error('Server Error')
+    logger.warning(traceback.format_exc())
+    return JSONResponse(
+        dict(message='server error'), status_code=HTTP_500_INTERNAL_SERVER_ERROR
+    )
--- app/main.py
+++ app/main.py
@@ -2,52 +2,18 @@ import uvicorn
 
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
-from starlette.responses import JSONResponse
-from starlette.status import (
-    HTTP_404_NOT_FOUND,
-    HTTP_422_UNPROCESSABLE_ENTITY,
-    HTTP_500_INTERNAL_SERVER_ERROR,
-)
+from starlette.status import HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
 
-from app.core.config import PROJECT_NAME, VERSION
+from app.core.config import PROJECT_NAME, VERSION, API_PREFIX
 from app.core.events import startup, shutdown
+from app.exceptions import (
+    page_not_found_handler,
+    bad_request_handler,
+    validation_exception_handler,
+    server_error_handler,
+    BadRequestException,
+)
 from app.api import api_router
-from app.exceptions import BadRequestException
-
-app = FastAPI(title=PROJECT_NAME)
-
-app.include_router(api_router)
-
-
-@app.exception_handler(HTTP_404_NOT_FOUND)
-async def page_not_found(request, exc):
-    """page not found"""
-
-    return JSONResponse(dict(message='page not found'), status_code=exc.status_code)
-
-
-@app.exception_handler(BadRequestException)
-async def bad_request_handler(request, exc):
-    """Bad Request"""
-    return JSONResponse(dict(message=exc.detail), status_code=exc.status_code)
-
-
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request, exc):
-    """验证前端输入实体"""
-    errors = exc.errors()
-    message = errors[0]['msg']
-
-    return JSONResponse(
-        dict(message=message), status_code=HTTP_422_UNPROCESSABLE_ENTITY
-    )
-
-
-@app.exception_handler(HTTP_500_INTERNAL_SERVER_ERROR)
-async def server_error(request, exc):
-    return JSONResponse(
-        dict(message='server error'), status_code=HTTP_500_INTERNAL_SERVER_ERROR
-    )
 
 
 def get_application() -> FastAPI:
@@ -56,6 +22,17 @@ def get_application() -> FastAPI:
 
     application.add_event_handler('startup', startup)
     application.add_event_handler('shutdown', shutdown)
+
+    application.add_exception_handler(HTTP_404_NOT_FOUND, page_not_found_handler)
+    application.add_exception_handler(BadRequestException, bad_request_handler)
+    application.add_exception_handler(
+        RequestValidationError, validation_exception_handler
+    )
+    application.add_exception_handler(
+        HTTP_500_INTERNAL_SERVER_ERROR, server_error_handler
+    )
+
+    application.include_router(api_router, prefix=API_PREFIX)
     return application
 
 
