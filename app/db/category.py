c149f425b640827526b65343b4bceecd528418bf
huangkai <h1770360848@outlook.com>
2020-02-08 15:08:27 +0800

    feat: 修改数据模型

--- app/db/category.py
+++ app/db/category.py
@@ -13,12 +13,10 @@ class ProductCategory(Model, ModelTimeMixin):
     id = fields.IntField(pk=True)
     parent_id = fields.IntField(index=True, default=0, description='父分类, 0表示一级分类')
     cat_name = fields.CharField(max_length=64, unique=True, description='分类名')
-    cat_level = fields.SmallIntField(
-        index=True, description='分类等级  0 --> 1级;  1 --> 2级'
-    )
+    cat_level = fields.SmallIntField(index=True, description='分类等级,0->1级,1->2级')
     cat_keywords = fields.CharField(max_length=255, null=True, description='分类关键词')
     cat_icon = fields.CharField(max_length=255, null=True, description='分类图标')
-    cat_desc = fields.TextField(null=True, description='分类描述')
+    cat_desc = fields.CharField(max_length=255, null=True, description='分类描述')
 
     class Meta:
         table = 'product_category'
@@ -32,7 +30,7 @@ class ProductCategorySpec(Model, ModelTimeMixin):
     id = fields.IntField(pk=True)
     spec_name = fields.CharField(max_length=64, description='分类规格名称')  # 颜色 ...
     join_select = fields.BooleanField(index=True, description='是否可以筛选')
-    spec_type = fields.IntField(index=True, description='规格类型  1 销售规格属性 2 展示属性')
+    spec_type = fields.SmallIntField(index=True, description='规格类型  1 销售规格属性 2 展示属性')
     cat_id = fields.IntField(index=True, description='商品分类id')
 
     class Meta:
